e58cc690e11bff71eb23b987e262f3448ce20a67
Abeera67 <abeeraishaque67@gmail.com>
2023-04-28 19:48:50 +0500

    add

--- javascript/header.js
+++ javascript/header.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
-  $(".menu-icon").on("click", function() {
-        $("nav ul").toggleClass("showing");
-  });
-});
-
-// Scrolling Effect
-
-$(window).on("scroll", function() {
-  if($(window).scrollTop()) {
-        $('nav').addClass('black');
-  }
-
-  else {
-        $('nav').removeClass('black');
-  }
-})
+      // executes when HTML-Document is loaded and DOM is ready
+    
+    
+      /*
+      ################
+      Add navbar background color when scrolled
+      */
+      $(window).scroll(function() {
+        if ($(window).scrollTop() > 56) {
+          $(".navbar").addClass("bg-dark");
+        } else {
+          $(".navbar").removeClass("bg-dark");
+        }
+      });
+      // If Mobile, add background color when toggler is clicked
+      $(".navbar-toggler").click(function() {
+        if (!$(".navbar-collapse").hasClass("show")) {
+          $(".navbar").addClass("bg-dark");
+        } else {
+          if ($(window).scrollTop() < 56) {
+            $(".navbar").removeClass("bg-dark");
+          } else {
+          }
+        }
+      });
+      // ############
+    
+      // document ready
+    });
